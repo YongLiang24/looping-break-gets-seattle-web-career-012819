9e56d91be67c7afaed8b34cb8bcae1d148d35810
yong liang <yongliang24@gmail.com>
2019-01-19 04:27:04 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,9 @@
 
 def levitation_quiz
 	#your code here
+  answer = gets.chomp
+  while answer != "Wingardium Leviosa"
+    puts "What is the spell that enacts levitation?"
+  end
+  puts "You passed the quiz!"
 end
-
-
